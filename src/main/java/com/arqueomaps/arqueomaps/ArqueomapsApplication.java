9d34ea758f7ea926312e07e886ba88dc5432568d
Mancipox <smancipeparedes@gmail.com>
2019-04-18 10:38:18 -0500

    Initial commit

---
+++ src/main/java/com/arqueomaps/arqueomaps/ArqueomapsApplication.java
@@ -0,0 +1,13 @@
+package com.arqueomaps.arqueomaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqueomapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqueomapsApplication.class, args);
+	}
+
+}
